2ab545befd42bc1d0475661639065de3d1b34a1d
edcleciomicael <edcleciomicael@gmail.com>
2026-05-13 15:03:26 -0300

    Novas pastas para criar uma estrutura MVC

---
+++ views/teste.php
@@ -0,0 +1,34 @@
+<!-- CARDS SUPERIORES -->
+<div class="finance-grid">
+    <div class="summary-card">
+        <div style="font-size:32px;">💰</div>
+        <div>
+            <h5>RECEITA TOTAL</h5>
+            <h2 style="color:#4ade80;">R$ <?= number_format($totalEntradas, 2, ',', '.') ?></h2>
+        </div>
+    </div>
+
+    <div class="summary-card">
+        <div style="font-size:32px;">💵</div>
+        <div>
+            <h5>DINHEIRO</h5>
+            <h2>R$ 0,00</h2>
+        </div>
+    </div>
+
+    <div class="summary-card">
+        <div style="font-size:32px;">📱</div>
+        <div>
+            <h5>PIX</h5>
+            <h2>R$ 0,00</h2>
+        </div>
+    </div>
+
+    <div class="summary-card">
+        <div style="font-size:32px;">💳</div>
+        <div>
+            <h5>CARTÃO</h5>
+            <h2 style="color:#4ade80;">R$ <?= number_format($totalEntradas, 2, ',', '.') ?></h2>
+        </div>
+    </div>
+</div>
